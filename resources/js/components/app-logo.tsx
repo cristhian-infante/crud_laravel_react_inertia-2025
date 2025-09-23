672f645bf083c205d0619d0ff5f34a3230b1e3bf
Cristhian Infante <cristhian.infante117@gmail.com>
2025-09-23 16:52:08 -0500

    Initial commit

---
+++ resources/js/components/app-logo.tsx
@@ -0,0 +1,16 @@
+import AppLogoPage from './logoPrueba';
+
+export default function AppLogo() {
+    return (
+        <>
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md text-sidebar-primary-foreground">                
+                <AppLogoPage />
+            </div>
+            <div className="ml-1 grid flex-1 text-left text-sm">
+                <span className="mb-0.5 truncate leading-tight font-semibold">
+                    CRUD
+                </span>
+            </div>
+        </>
+    );
+}
